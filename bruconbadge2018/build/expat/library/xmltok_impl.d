685c1a86f199cc05d44055782a82a0a0ee1484f3
Jegeva <jejeva.bxl@gmail.com>
2018-10-06 23:18:53 +0200

    cleanup

--- bruconbadge2018/build/expat/library/xmltok_impl.d
+++
@@ -1,3 +0,0 @@
-library/xmltok_impl.o: \
- /mnt/data/jg/work/brucon/esp32docker/esp-idf/components/expat/library/xmltok_impl.c
-/mnt/data/jg/work/brucon/esp32docker/esp-idf/components/expat/library/xmltok_impl.c:
--- bruconbadge2018/build/expat/library/xmltok_ns.d
+++
@@ -1,3 +0,0 @@
-library/xmltok_ns.o: \
- /mnt/data/jg/work/brucon/esp32docker/esp-idf/components/expat/library/xmltok_ns.c
-/mnt/data/jg/work/brucon/esp32docker/esp-idf/components/expat/library/xmltok_ns.c:
